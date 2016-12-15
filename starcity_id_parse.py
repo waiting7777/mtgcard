05f09e1fd137fd96207beb1bbe3c988553dcb36b
Waiting7777 <waiting0819@msn.com>
2016-12-15 11:57:09 +0800

    add starcity card id

--- starcity_id_parse.py
+++ starcity_id_parse.py
@@ -16,3 +16,34 @@ if sys.argv[1] not in card_set:
 	print('please input supported card_set')
 	print(card_set)
 	exit()
+
+w = codecs.open('starcity/kld/cardid.csv', 'w', 'utf8')
+w.write('CardName, StarCardId\n')
+
+set = sys.argv[1]
+
+dic = {}
+
+for rarity in star_city_rarity:
+
+	for i in range(star_city_page[set][rarity]):
+
+		f = codecs.open('starcity/%s/%s_%d.html'%(set, rarity, i+1), 'r', 'utf8')
+
+		content = f.read()
+
+		soup = BeautifulSoup(content, 'html.parser')
+
+		for item in soup.select('.card_popup'):
+			print(item.text, item['href'][-7:])
+			dic[item.text[1:]] = item['href'][-7:]
+
+		f.close()
+
+for key in sorted(dic):
+	print(key, dic[key])
+	w.write(key + ', ' + dic[key] + '\n')
+
+
+	
+
